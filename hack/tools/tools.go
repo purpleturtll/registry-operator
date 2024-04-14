71c7933d201accd5c82b19cc5980305f7348cd49
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-04-14 15:44:05 +0200

    chore(tools): add tools versioning (#21)
    ## What this PR does / why we need it:
    <!--
    What code changes are made?
    What problem does this PR addresses, or what feature this PR adds?
    -->
    Added few minor hacks and tweaks to tooling, so we can benefit more from
    Renovate for automatic updates of tooling.
    
    ## Which issue(s) this PR resolves:
    <!--
    Usage: `Resolves #<issue number>`, or `Resolves <link to the issue>`.
    If PR is about `failing-tests`, please post the related tests in a
    comment and do not use `Resolves`
    -->
    N/A
    
    ## Special notes for your reviewer:
    <!-- Do you think reviewers should focus on any particular parts of
    code? -->
    N/A
    
    ## Additional documentation e.g., enhancement proposals, usage docs,
    etc.:
    <!-- This section can be blank if this pull request does not require a
    release note. -->
    N/A
    
    ---------
    
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

---
+++ hack/tools/tools.go
@@ -0,0 +1,12 @@
+//go:build none
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/operator-framework/operator-registry/cmd/opm"
+	_ "github.com/operator-framework/operator-sdk/cmd/operator-sdk"
+	_ "k8s.io/kubelet"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+	_ "sigs.k8s.io/kustomize/kustomize/v5"
+)
